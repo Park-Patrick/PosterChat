fb1f3a36ecab60d0ed2f5d9fac03b1051dbf9462
park-patrick <parkpatrickj@gmail.com>
2020-02-21 01:17:58 -0500

    add more pages

--- poster/urls.py
+++ poster/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import (
-	PostListView
+	PostListView,
+	PostListView2,
+	PostListView3,
 )
 
 app_name = 'poster'
@@ -8,5 +10,6 @@ app_name = 'poster'
 urlpatterns = [
 #local : http://127.0.0.1:8000/
 	path('', PostListView.as_view(), name='post_list'),
-	# path('1/', PostListView.as_view(), name='post_list')
+	path('1/', PostListView2.as_view(), name='post_list_2'),
+	path('2/', PostListView3.as_view(), name='post_list_3')
 ]
--- poster/views.py
+++ poster/views.py
@@ -10,3 +10,14 @@ class PostListView(ListView):
 	template_name = "poster/post_list.html"
 	queryset = Post.objects.all()
 	context_object_name = 'posts'
+
+class PostListView2(ListView):
+	template_name = "poster/post_list_2.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+
+class PostListView3(ListView):
+	template_name = "poster/post_list_3.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
