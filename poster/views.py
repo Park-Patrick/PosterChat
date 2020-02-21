e062f0e85a692df02d611385f7c622f2053ed708
park-patrick <parkpatrickj@gmail.com>
2020-02-21 04:06:18 -0500

    add 50 html and others

--- poster/views.py
+++ poster/views.py
@@ -11,13 +11,262 @@ class PostListView(ListView):
 	queryset = Post.objects.all()
 	context_object_name = 'posts'
 
-class PostListView2(ListView):
-	template_name = "poster/post_list_2.html"
+class PostListView00(ListView):
+	template_name = "poster/post_list00.html"
 	queryset = Post.objects.all()
 	context_object_name = 'posts'
 
+class PostListView1(ListView):
+	template_name = "poster/post_list1.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView2(ListView):
+	template_name = "poster/post_list2.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
 
 class PostListView3(ListView):
-	template_name = "poster/post_list_3.html"
+	template_name = "poster/post_list3.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView4(ListView):
+	template_name = "poster/post_list4.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView5(ListView):
+	template_name = "poster/post_list5.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView6(ListView):
+	template_name = "poster/post_list6.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView7(ListView):
+	template_name = "poster/post_list7.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView8(ListView):
+	template_name = "poster/post_list8.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView9(ListView):
+	template_name = "poster/post_list9.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView10(ListView):
+	template_name = "poster/post_list10.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView11(ListView):
+	template_name = "poster/post_list11.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView12(ListView):
+	template_name = "poster/post_list12.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView13(ListView):
+	template_name = "poster/post_list13.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView14(ListView):
+	template_name = "poster/post_list14.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView15(ListView):
+	template_name = "poster/post_list15.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView16(ListView):
+	template_name = "poster/post_list16.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView17(ListView):
+	template_name = "poster/post_list17.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView18(ListView):
+	template_name = "poster/post_list18.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView19(ListView):
+	template_name = "poster/post_list19.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView20(ListView):
+	template_name = "poster/post_list20.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView21(ListView):
+	template_name = "poster/post_list21.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView22(ListView):
+	template_name = "poster/post_list22.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView23(ListView):
+	template_name = "poster/post_list23.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView24(ListView):
+	template_name = "poster/post_list24.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView25(ListView):
+	template_name = "poster/post_list25.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView26(ListView):
+	template_name = "poster/post_list26.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView27(ListView):
+	template_name = "poster/post_list27.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView28(ListView):
+	template_name = "poster/post_list28.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView29(ListView):
+	template_name = "poster/post_list29.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView30(ListView):
+	template_name = "poster/post_list30.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView31(ListView):
+	template_name = "poster/post_list31.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView32(ListView):
+	template_name = "poster/post_list32.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView33(ListView):
+	template_name = "poster/post_list33.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView34(ListView):
+	template_name = "poster/post_list34.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView35(ListView):
+	template_name = "poster/post_list35.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView36(ListView):
+	template_name = "poster/post_list36.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView37(ListView):
+	template_name = "poster/post_list37.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView38(ListView):
+	template_name = "poster/post_list38.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView39(ListView):
+	template_name = "poster/post_list39.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView40(ListView):
+	template_name = "poster/post_list40.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView41(ListView):
+	template_name = "poster/post_list41.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView42(ListView):
+	template_name = "poster/post_list42.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView43(ListView):
+	template_name = "poster/post_list43.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView44(ListView):
+	template_name = "poster/post_list44.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView45(ListView):
+	template_name = "poster/post_list45.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView46(ListView):
+	template_name = "poster/post_list46.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView47(ListView):
+	template_name = "poster/post_list47.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView48(ListView):
+	template_name = "poster/post_list48.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView49(ListView):
+	template_name = "poster/post_list49.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView50(ListView):
+	template_name = "poster/post_list50.html"
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+
+class PostListView51(ListView):
+	template_name = "poster/post_list51.html"
 	queryset = Post.objects.all()
 	context_object_name = 'posts'
