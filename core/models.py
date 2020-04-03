31309f328c3600a1413483ecb5de4240b45093f0
serant <seran.thirugnanam@gmail.com>
2020-04-02 20:10:04 -0400

    Added tests to validate custom user moder

--- core/models.py
+++ core/models.py
@@ -6,7 +6,14 @@ from PIL import Image
 
 
 class UserManager(BaseUserManager):
+    """Implements Django user manager to use the custom user in PosterChat
+
+    Django expects UserManager interface as such:
+    https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#writing-a-manager-for-a-custom-user-model
+    """
+
     def create_user(self, email, first_name, last_name, username, password=None, commit=True):
+
         if not email:
             raise ValueError(trans("Users must have an email address"))
 
@@ -38,33 +45,13 @@ class UserManager(BaseUserManager):
         return user
 
 
-"""
-So when a conference is CRU, we need to create three groups:
-    1. Organizers
-    2. Attendees
-    3. Guests
-
-- Organizers (can create/delete posters)
-    - Can add/delete/change/view their conferences
-    - Can add/delete/change/view their conference's posters
-    - Can add/delete/change/view their conference's poster's comments
-    - Can add/change/view their own accounts
-
-
-- Attendees (a conference object hold these, can comment on any poster in the conference)
-    - Can delete/change/view their own poster
-    - Can add/view their own poster's comments
-    - Can add/change/view their own accounts
-
-- Guests (can view posters only)
-    - Can view conferences
-    - Can view posters
-    - Can view comments
-    - Can add/change/view their own accounts
-"""
-
-
 class User(AbstractBaseUser, PermissionsMixin):
+    """Implements Django user for use in PosterChat
+
+    Django expects User models to support the following interface
+    https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#specifying-a-custom-user-model
+    """
+
     email = models.EmailField(verbose_name=trans(
         "email address"), max_length=255, unique=True)
 
@@ -99,20 +86,17 @@ class User(AbstractBaseUser, PermissionsMixin):
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}".strip()
 
+    def get_short_name(self):
+        return self.first_name.strip()
+
     def __str__(self):
         return f"{self.get_full_name()} <{self.email}>"
 
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
     def save(self, *args, **kwargs):
-        super().save()
-
         img = Image.open(self.avatar.path)
 
         if img.height > 200 or img.width > 200:
             img.thumbnail((200, 200))
             img.save(self.avatar.path)
+
+        super().save()
